f13467035add390a72200fa7689703776c23579f
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2024-02-18 22:04:00 +0530

    add: pagination

--- controller/hotelsController.js
+++ controller/hotelsController.js
@@ -3,15 +3,29 @@ const mongoose = require('mongoose');
 const Hotel = require('../models/hotelSchema');
 const PopularHotel = require('../models/popularHotelsSchema');
 
+
 exports.getAllHotels = async (req, res) => {
   try {
-    const hotels = await Hotel.find();
-    res.json(hotels);
+    const page = parseInt(req.query.page) || 1; // Parse the page query parameter or default to 1
+    const pageSize = 10; // Number of documents per page
+
+    const totalHotels = await Hotel.countDocuments();
+    const totalPages = Math.ceil(totalHotels / pageSize);
+
+    const hotels = await Hotel.find()
+      .skip((page - 1) * pageSize) // Skip documents based on the page number
+      .limit(pageSize); // Limit the number of documents returned
+
+    res.json({
+      hotels,
+      currentPage: page,
+      totalPages
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-    // res.send("hotel all");
 };
+
 exports.addHotel = async (req, res) => {
 //   try {
 //     const hotels = await Hotel.find();
